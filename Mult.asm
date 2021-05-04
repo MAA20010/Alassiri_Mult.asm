101e13b10b0172ba19399c778db8440868366176
AlassSecret <81076981+AlassSecret@users.noreply.github.com>
2021-05-04 21:57:26 +1000

    Add files via upload

---
+++ Mult.asm
@@ -0,0 +1,48 @@
+// Mohammed Alassiri
+// I validated the hack program with the script and the output file = cmp file. 
+// I also tested it manually to understand it better.
+// REFERENCES:
+// https://zhongchuyun.gitbooks.io/nand2tetris/content/chapter_4.html I checked this when 
+// I was looking on how to make a loop
+
+
+@2
+M=0 // starts r2 as 0, because it adds value of r1 to it r0 times.
+
+@0
+D=M
+@END // go to end if value = 0 
+D;JEQ // JLe was broken, JEQ was the only option that worked, it would sometimes give a value below 0???
+
+@1
+D=M 
+@END // go to end if value = 0 
+D;JEQ 
+
+(Mult) // its simply a while loop.
+
+	@1
+	D=M 
+
+	@2
+	M=D+M // Adds r1 value to the value of r2 ( will run r0 times)
+	@END 
+	M < 
+
+	@0
+	M=M-1 // reduces the value of r0 by 1 each time it runs the function.
+	D=M // this was broken for some reason, i deleted it then typed it again, and it worked???????
+	@Mult // jump to the start of the loop IF r0 is bigger than 0.
+	D;JGT
+	
+
+(END)
+	@END
+	0;JMP
+
+
+
+
+
+
+
